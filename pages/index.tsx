da045cf7d829fdc7ba118b9b64d943151b2091b7
Onur Çağan <onurcagann@gmail.com>
2023-04-18 22:48:01 +0300

    Test the new beta Cache API of next

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,21 @@ export default function Index({ time }: InferGetServerSidePropsType<typeof getSe
     <main>
       <h1>SSR Caching with Next.js</h1>
       <time dateTime={time}>{time}</time>
-      <p>Refresh the page, s-maxage=10s, stale-while-revalidate=60s.</p>
+      <p>New Cache API test with revalidate:10</p>
+      {/* <p>Refresh the page, s-maxage=10s, stale-while-revalidate=60s.</p> */}
     </main>
   )
 }
 
 export const getServerSideProps: GetServerSideProps<{ time: string }> = async ({ res }) => {
   res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=60')
+  const timeResponse = await fetch('https://timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam', {
+    next: { revalidate: 30 },
+  }).then((res) => res.json())
+
   return {
     props: {
-      time: new Date().toISOString(),
+      time: timeResponse.dateTime,
     },
   }
 }
